5201af1f26545edf3a6ea3b6460d88eacad6d38b
stephensappor <stephensappor5@gmail.com>
2024-06-28 09:29:17 +0000

    React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Recipes from "./pages/Recipes";
 import Welcome from "./pages/Welcome";
+import Categories from "./pages/Categories";
+
+const router = createBrowserRouter([
+  { path: '/', element: <Welcome /> },
+  { path: '/recipes', element: <Recipes /> },
+  { path: '/categories', element: <Categories /> }
+
+]);
+
 function App() {
   return (
     <>
-    <Welcome />
+      <RouterProvider router={router} />
     </>
+
   )
 }
+
 
 export default App
